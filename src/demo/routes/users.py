311d107cf3649f8ced9cb37b2f4ce29afc55cfd2
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-29 19:26:00 +0200

    fix: exception inception

--- src/demo/routes/users.py
+++ src/demo/routes/users.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from jose import jwt
+from jose import jwt, JWTError
 
 from demo.lib.jwt import ALGORITHM, SECRET_KEY
 from demo.lib.oauth2 import oauth2_bearer
@@ -27,19 +27,22 @@ async def get_current_user(
 ):
     try:
         payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        user_id: int = payload.get("id")
-        if username is None or user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate user",
-            )
-        return {"username": username, "id": user_id}
-    except Exception:
+    except JWTError as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate user",
+        ) from err
+
+    username: str = payload.get("sub")
+    user_id: int = payload.get("id")
+
+    if username is None or user_id is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate user",
         )
+
+    return {"username": username, "id": user_id}
 
 
 user_dependency = Annotated[dict, Depends(get_current_user)]
