3d306af83347f397d309709c77dcb422a5babb82
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-29 19:26:35 +0200

    fix: comma

--- src/demo/routes/auth.py
+++ src/demo/routes/auth.py
@@ -43,7 +43,8 @@ async def login_for_access_token(
     user = await authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
         )
     token = await create_access_token(
         username=user["username"],
